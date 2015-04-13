902f878f0ef2a53bef3aa0cf367e118faa768cdf
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2015-04-13 07:27:09 -0400

    added function_return_type

--- include/echo/type_traits.h
+++ include/echo/type_traits.h
@@ -21,5 +21,36 @@ template <class T>
 struct ConvertToConstPointer<const T*> {
   using type = const T*;
 };
+
+//////////////////////////
+// function_return_type //
+//////////////////////////
+
+namespace detail {
+namespace type_traits {
+template <class T>
+struct FunctionReturnType {};
+
+template <class Return, class... Args>
+struct FunctionReturnType<Return(Args...)> {
+  using type = Return;
+};
+
+template <class Return, class Record, class... Args>
+struct FunctionReturnType<Return (Record::*)(Args...)> {
+  using type = Return;
+};
+
+template <class Return, class Record, class... Args>
+struct FunctionReturnType<Return (Record::*)(Args...) const> {
+  using type = Return;
+};
+}
+}
+
+template <class FunctionPtr>
+using function_return_type =
+    typename detail::type_traits::FunctionReturnType<FunctionPtr>::type;
+
 }  // namespace type_traits
 }  // namespace echo
--- unittest/type_traits_test.cpp
+++ unittest/type_traits_test.cpp
@@ -1,9 +1,10 @@
-#pragma once
-
 #include <echo/type_traits.h>
 #include <catch.hpp>
 
+using namespace echo;
 using namespace echo::type_traits;
+
+double f1(int) { return 2.7; }
 
 TEST_CASE("type_traits") {
   using P1 = ConvertToConstPointer<int*>::type;
@@ -11,4 +12,7 @@ TEST_CASE("type_traits") {
 
   using P2 = ConvertToConstPointer<const int*>::type;
   REQUIRE(std::is_same<P2, const int*>::value);
+
+  using R1 = type_traits::function_return_type<decltype(f1)>;
+  REQUIRE(std::is_same<R1, double>::value);
 }
