a0b811114da461dea543b0e7c6c058c239699550
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2015-05-04 10:05:01 -0400

    updates for numeric array

--- include/echo/type_traits.h
+++ include/echo/type_traits.h
@@ -45,12 +45,79 @@ template <class Return, class Record, class... Args>
 struct FunctionReturnType<Return (Record::*)(Args...) const> {
   using type = Return;
 };
+
+template <class Return, class Record, class... Args>
+struct FunctionReturnType<Return (Record::*)(Args...) volatile> {
+  using type = Return;
+};
+
+template <class Return, class Record, class... Args>
+struct FunctionReturnType<Return (Record::*)(Args...) const volatile> {
+  using type = Return;
+};
+
 }
 }
 
 template <class FunctionPtr>
 using function_return_type =
     typename detail::type_traits::FunctionReturnType<FunctionPtr>::type;
+
+/////////////////////////
+// functor_return_type //
+/////////////////////////
+
+template <class Functor>
+using functor_return_type =
+    function_return_type<decltype(&Functor::operator())>;
+
+////////////////////
+// function_arity //
+////////////////////
+
+namespace detail { namespace type_traits {
+template <class T>
+struct FunctionArity {};
+
+template <class Return, class... Args>
+struct FunctionArity<Return(Args...)> {
+  static constexpr int value = sizeof...(Args);
+};
+
+template <class Return, class Record, class... Args>
+struct FunctionArity<Return (Record::*)(Args...)> {
+  static constexpr int value = sizeof...(Args);
+};
+
+template <class Return, class Record, class... Args>
+struct FunctionArity<Return (Record::*)(Args...) const> {
+  static constexpr int value = sizeof...(Args);
+};
+
+template <class Return, class Record, class... Args>
+struct FunctionArity<Return (Record::*)(Args...) volatile> {
+  static constexpr int value = sizeof...(Args);
+};
+
+template <class Return, class Record, class... Args>
+struct FunctionArity<Return (Record::*)(Args...) const volatile> {
+  static constexpr int value = sizeof...(Args);
+};
+}}
+
+template<class FunctionPtr>
+constexpr int function_arity() {
+  return detail::type_traits::FunctionArity<FunctionPtr>::value;
+}
+
+///////////////////
+// functor_arity //
+///////////////////
+
+template<class Functor>
+constexpr int functor_arity() {
+  return function_arity<decltype(&Functor::operator())>();
+}
 
 }  // namespace type_traits
 }  // namespace echo
--- unittest/type_traits_test.cpp
+++ unittest/type_traits_test.cpp
@@ -6,13 +6,31 @@ using namespace echo::type_traits;
 
 double f1(int) { return 2.7; }
 
+struct A {
+  int f1(int);
+  int f2(int) const;
+  int f3(int) volatile;
+  int f4(int) const volatile;
+};
+
 TEST_CASE("type_traits") {
   using P1 = ConvertToConstPointer<int*>::type;
-  REQUIRE(std::is_same<P1, const int*>::value);
+  CHECK(std::is_same<P1, const int*>::value);
 
   using P2 = ConvertToConstPointer<const int*>::type;
-  REQUIRE(std::is_same<P2, const int*>::value);
+  CHECK(std::is_same<P2, const int*>::value);
 
   using R1 = type_traits::function_return_type<decltype(f1)>;
-  REQUIRE(std::is_same<R1, double>::value);
+  CHECK(std::is_same<R1, double>::value);
+
+  CHECK(std::is_same<type_traits::function_return_type<decltype(&A::f1)>,
+                       int>::value);
+  CHECK(std::is_same<type_traits::function_return_type<decltype(&A::f2)>,
+                       int>::value);
+  CHECK(std::is_same<type_traits::function_return_type<decltype(&A::f3)>,
+                       int>::value);
+  CHECK(std::is_same<type_traits::function_return_type<decltype(&A::f4)>,
+                       int>::value);
+
+  CHECK(type_traits::function_arity<decltype(f1)>() == 1);
 }
