0a29bab90166c7174262ebac8684b3ad5df37b3a
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2015-06-28 10:01:13 -0400

    added or operator

--- include/echo/variadic_operator.h
+++ include/echo/variadic_operator.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#define DETAIL_NS detail_variadic_operator
+
 namespace echo {
 
 ///////////
 // and_c //
 ///////////
 
-namespace detail {
-namespace variadic_operator {
+namespace DETAIL_NS {
 template <bool... Values>
 struct and_c_impl {
   static constexpr bool value = true;
@@ -18,10 +19,32 @@ struct and_c_impl<ValueFirst, ValuesRest...> {
   static constexpr bool value = ValueFirst && and_c_impl<ValuesRest...>::value;
 };
 }
-}
 
 template <bool... Values>
 constexpr bool and_c() {
-  return detail::variadic_operator::and_c_impl<Values...>::value;
+  return DETAIL_NS::and_c_impl<Values...>::value;
 }
 }
+
+//////////
+// or_c //
+//////////
+
+namespace DETAIL_NS {
+template <bool... Values>
+struct or_c_impl {
+  static constexpr bool value = false;
+};
+
+template <bool ValueFirst, bool... ValuesRest>
+struct or_c_impl<ValueFirst, ValuesRest...> {
+  static constexpr bool value = ValueFirst || or_c_impl<ValuesRest...>::value;
+};
+}
+
+template <bool... Values>
+constexpr bool or_c() {
+  return DETAIL_NS::or_c_impl<Values...>::value;
+}
+
+#undef DETAIL_NS
